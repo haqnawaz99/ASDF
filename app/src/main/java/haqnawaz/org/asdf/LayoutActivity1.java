6c8eeda6248045ca2b14f882b73e0df582fcf54c
Haq Nawaz <haqnawaz99@yahoo.com>
2021-10-18 08:25:32 +0500

    GUI Implmented

---
+++ app/src/main/java/haqnawaz/org/asdf/LayoutActivity1.java
@@ -0,0 +1,14 @@
+package haqnawaz.org.asdf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout1);
+    }
+}
